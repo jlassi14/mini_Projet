bf40faa7f2d80140d2b800680b13f7f810d2bad7
jlassi14 <omarjlassi454@gmail.com>
2023-08-02 15:29:39 +0200

    styling

--- Components/Details.js
+++ Components/Details.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet , TouchableOpacity, ImageBackground, ScrollView} from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useSelector, useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const Details = () => {
   // Get the movie object from the route parameters
@@ -50,77 +51,136 @@ const Details = () => {
     }
   };
 
-  return (
-    <View style={styles.container}>
-      {/* Display the movie poster */}
-      <Image source={{ uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}` }} style={styles.poster} />
-      {/* Display the movie title with a star icon if it's in the favorite list */}
-      <View style={styles.titleContainer}>
-        {isMovieFavorite() && <Icon name="star" size={20} color="red" />}
-        <Text style={styles.title}>{movie.title}</Text>
-      </View>
-      {/* Display movie genres, release date, duration, and average rating */}
-      {movie.genres ? (
-        <Text style={styles.details}>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</Text>
-      ) : (
-        <Text style={styles.details}>Genres: Not available</Text>
-      )}
-      <Text style={styles.details}>Release Date: {movie.release_date}</Text>
-      <Text style={styles.details}>Duration: {movie.runtime} minutes</Text>
-      <Text style={styles.details}>Average Rating: {movie.vote_average}</Text>
-      {/* Display movie overview */}
-      <Text style={styles.overview}>{movie.overview}</Text>
 
-      {/* Display "Add to Favorites" or "Remove from Favorites" text based on whether the movie is in the favorite list */}
-      <View style={styles.favoriteButtonContainer}>
-        {isMovieFavorite() ? (
-          <Text style={styles.favoriteText} onPress={addToFavorites}>
-            Remove from Favorites
-          </Text>
-        ) : (
-          <Text style={styles.favoriteText} onPress={addToFavorites}>
-            Add to Favorites
-          </Text>
-        )}
+
+
+  return (
+    
+    <View style={styles.container}>
+     
+      {/* Image background (cover image) */}
+      <ImageBackground
+        source={{ uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}` }}
+        style={styles.posterBackground}
+        blurRadius={3}
+      >
+        {/* Display the movie title with a star icon if it's in the favorite list */}
+        <View style={styles.titleContainer}>
+          {isMovieFavorite() && <Icon name="star" size={20} color="red" />}
+          <View>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.details}>Duration: {movie.runtime} minutes</Text>
+            <Text style={styles.details}>Release Date: {movie.release_date}</Text>
+            {movie.genres ? (
+              <Text style={styles.details}>Genres: {movie.genres.map((genre) => genre.name).join(', ')}</Text>
+            ) : (
+              
+
+              <View  style={styles.favoriteButtonContainer}>
+              <Text style={styles.details}>Genres: Not available</Text>
+              </View>
+            )}
+          </View>
+        </View>
+  
+        {/* Display "Add to Favorites" or "Remove from Favorites" text based on whether the movie is in the favorite list */}
+        <TouchableOpacity style={styles.heartIconContainer} onPress={addToFavorites}>
+        <Icon name={isMovieFavorite() ? 'heart' : 'heart-outline'} size={30} color="#FF0000" />
+      </TouchableOpacity>
+      </ImageBackground>
+  
+      {/* Display movie overview */}
+     
+
+      <View style={styles.overviewContainer}>
+        <Text style={styles.overviewTitle}>Overview</Text>
+        <Text style={styles.overview}>{movie.overview}</Text>
       </View>
     </View>
   );
+  
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     padding: 16,
   },
-  poster: {
-    width: 200,
-    height: 300,
-    marginBottom: 10,
+  posterBackground: {
+    width: '100%',
+    height: '75%', // Set the background height to 75% of the screen
+    alignItems: 'center',
+    borderBottomLeftRadius: 20, 
+    borderBottomRightRadius: 20, 
+   
   },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginTop: 140,
+    justifyContent: 'center', // Center the text horizontally
   },
   title: {
-    fontSize: 20,
+    marginBottom:10,
+    fontSize: 29,
     fontWeight: 'bold',
-    marginBottom: 10,
-    marginLeft: 5,
+    color: 'white', // Add color to the movie title
+   // marginLeft: 10,
   },
   details: {
+    fontSize: 20,
     marginBottom: 5,
+    color: 'white', // Add color to the movie details text
+   // fontWeight: 'bold',
+  },
+  detailsValue: {
+    fontWeight: 'bold',
+    color: 'white', // Add color to the movie details value (e.g., duration, release date, genres)
+  },
+  overviewContainer: {
+    marginTop: -90,
+    padding: 16,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)', // Semi-transparent background
+    borderRadius: 8,
+    width: '100%',
+  },
+  overviewTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'left',
+    color: 'black',
   },
   overview: {
+    fontSize: 16,
     textAlign: 'center',
-    marginBottom: 10,
+    color: 'black',
   },
+
   favoriteButtonContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background for the button
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
     marginBottom: 10,
+    width:230,
+    marginTop:10
   },
   favoriteText: {
-    color: 'blue',
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+  },
+  heartIconContainer: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+   // backgroundColor: 'rgba(255, 0, 0, 0.8)',
+    borderRadius: 20,
+    padding: 8,
   },
 });
+
+
 
 export default Details;
--- Components/Home.js
+++ Components/Home.js
@@ -1,9 +1,11 @@
 // Import required libraries
-import { View, TextInput, StyleSheet, FlatList, Text, Image, Keyboard, TouchableOpacity } from 'react-native';
+import { View, TextInput, StyleSheet, FlatList, Text, Image, Keyboard,Dimensions, TouchableOpacity } from 'react-native';
 import React, { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { useSelector, useDispatch } from 'react-redux';
+import Icon from 'react-native-vector-icons/Ionicons';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { Searchbar } from 'react-native-paper';
 const Home = () => {
   const API_KEY = 'a2f813058856fd9c644b17c154ceaf1f'; 
   const API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
@@ -44,36 +46,53 @@ const Home = () => {
   // Render individual movie items in the FlatList
   const renderMovieItem = ({ item }) => (
     <TouchableOpacity onPress={() => handleMoviePress(item)}>
-      <View style={{ marginBottom: 20, alignItems: 'center' }}>
-        <Image
-          source={{ uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}` }}
-          style={{ width: 200, height: 300 }}
-        />
-        <Text>{item.title}</Text>
-        <Text>Average Rating: {item.vote_average}</Text>
+    <View style={styles.movieContainer}>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}` }}
+        style={styles.movieImage}
+      />
+      <View style={styles.movieInfoContainer}>
+        <Text style={styles.movieTitle}>{item.title}</Text>
+        <Text style={styles.movieRating}>Average Rating: {item.vote_average}</Text>
       </View>
-    </TouchableOpacity>
+    </View>
+  </TouchableOpacity>
   );
 
   return (
+   
     <View style={styles.container}>
+      {/* Heart icon */}
+      <View style={styles.heartIconContainer}>
+        <TouchableOpacity  onPress={() =>  navigation.navigate('Favourites')}>
+          <Icon name="heart" size={24} color="#FF1493" />
+        </TouchableOpacity>
+      </View>
+
       {/* Search input */}
       <TextInput
         style={styles.input}
         placeholder="Search Movie"
+        placeholderTextColor="#888"
         onChangeText={handleSearch}
         onSubmitEditing={Keyboard.dismiss} // Close keyboard when pressing enter
       />
       {/* Movie list */}
       <FlatList
+      contentContainerStyle={{padding:30}}
+      horizontal={false}
+      numColumns={2}
         data={movies} // Use the movies data from the Redux store directly
         renderItem={renderMovieItem}
         keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={{ padding: 20 }}
+       // contentContainerStyle={{ padding: 20 }}
+       columnWrapperStyle={styles.rowContainer}
+     
       />
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
@@ -82,10 +101,49 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   input: {
-    height: 40,
+    height: 48,
     borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 24,
+    paddingLeft: 16,
+    fontSize: 16,
+    backgroundColor: '#fff',
     marginBottom: 12,
-    paddingHorizontal: 8,
+  },
+  heartIconContainer: {
+    flexDirection: 'row', // Stack icon and search bar vertically
+    justifyContent: 'flex-end', // Align the icon to the right
+    alignItems: 'center', // Center the icon vertically
+    marginBottom: 16, // Add some margin between icon and search bar
+  },
+  movieContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  movieImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 8,
+  },
+  movieInfoContainer: {
+    marginTop: 10,
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginLeft:10,
+  },
+  movieTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 8,
+  },
+  movieRating: {
+    fontSize: 14,
+    color: '#888',
+  },
+  rowContainer: {
+    justifyContent: 'space-between', // Add space between images
+    marginBottom: 20,
+    width: '100%', // Set the width of the row container to 100%
   },
 });
 
